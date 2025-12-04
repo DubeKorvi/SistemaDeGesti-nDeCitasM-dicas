839c8399a67368f4ee2c92bacc560cecbcc7934f
Dubenny Areche Castillo <Dubenny007@gmail.com>
2025-12-04 16:41:00 -0400

    Agregue los colores al form en base a Material skin

--- CapaPresentacioon/FMenuPrincipal.Designer.cs
+++ CapaPresentacioon/FMenuPrincipal.Designer.cs
@@ -28,84 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            panel1 = new Panel();
-            pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(23, 74);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(108, 69);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(pictureBox4);
-            panel1.Controls.Add(pictureBox3);
-            panel1.Controls.Add(pictureBox2);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Location = new Point(1, 2);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(294, 562);
-            panel1.TabIndex = 1;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Location = new Point(23, 167);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(108, 75);
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Location = new Point(23, 248);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(108, 69);
-            pictureBox3.TabIndex = 2;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Location = new Point(23, 345);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(108, 69);
-            pictureBox4.TabIndex = 3;
-            pictureBox4.TabStop = false;
             // 
             // FMenuPrincipal
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1068, 606);
-            Controls.Add(panel1);
+            ClientSize = new Size(1268, 704);
             Name = "FMenuPrincipal";
             Text = "FMenuPrincipal";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
-        private Panel panel1;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
     }
 }
--- CapaPresentacioon/FMenuPrincipal.cs
+++ CapaPresentacioon/FMenuPrincipal.cs
@@ -7,19 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
 
 namespace CapaPresentacioon
 {
-    public partial class FMenuPrincipal : Form
+    public partial class FMenuPrincipal : MaterialSkin.Controls.MaterialForm
     {
         public FMenuPrincipal()
         {
             InitializeComponent();
+
+            // Configurar MaterialSkin
+            var materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+
+            // --- CONFIGURACIÓN CON COLORES AZULES (similar al diseño de la imagen) ---
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(
+                MaterialSkin.Primary.Blue600,       // Azul principal
+                MaterialSkin.Primary.Blue800,       // Azul "dark" del menú y cabecera
+                MaterialSkin.Primary.Blue400,       // Azul más claro
+                MaterialSkin.Accent.LightBlue200,   // Accent suave estilo Windows 11
+                MaterialSkin.TextShade.WHITE        // Texto blanco para contrastar
+            );
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
