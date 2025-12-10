432234d5bfa23c8e63f74614cd34e3eedbc9e179
joseidy puello ciprian <joseidyp10@gmail.com>
2025-12-10 02:03:47 -0400

    hola

--- CapaNegocio/Clases/Credencial.cs
+++ CapaNegocio/Clases/Credencial.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using CapaDatos;
 
 namespace CapaNegocio.Clases
 {
@@ -13,5 +16,11 @@ namespace CapaNegocio.Clases
         public string Clave { get; set; }
         public string Rol { get; set; } // Secretaria, Medico
         public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        public DataTable Login(string usuario, string clave)
+        {
+            DCredencial d = new DCredencial();
+            return d.Login(usuario, clave);
+        }
     }
 }
